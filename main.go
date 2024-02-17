484d8631e5bb5b016e202c2e9bc03a789dd88c12
Maxime Petazzoni <maxime.petazzoni@bulix.org>
2024-02-16 18:14:46 -0800

    Add logging middleware to keep track of query activity

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 	"os"
 	"strings"
@@ -21,30 +20,49 @@ type Client struct {
 	LastEventId int       `json:"lastEventId"`
 }
 
-func emit(writer io.Writer, rc *http.ResponseController, id string, data string) error {
-	_, err := fmt.Fprintf(writer, "id: %s\n", id)
+type StreamResponseWriter struct {
+	http.ResponseWriter
+	controller *http.ResponseController
+	statusCode int
+}
+
+func (srw *StreamResponseWriter) WriteHeader(statusCode int) {
+	srw.statusCode = statusCode
+	srw.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (srw *StreamResponseWriter) WriteEvent(id string, data string) error {
+	_, err := fmt.Fprintf(srw, "id: %s\n", id)
 	if err != nil {
 		return err
 	}
 
 	sc := bufio.NewScanner(strings.NewReader(data))
 	for sc.Scan() {
-		_, err = fmt.Fprintf(writer, "data: %s\n", sc.Text())
+		_, err = fmt.Fprintf(srw, "data: %s\n", sc.Text())
 		if err != nil {
 			return err
 		}
 	}
-	_, err = fmt.Fprintf(writer, "\n")
+	_, err = fmt.Fprintf(srw, "\n")
 	if err != nil {
 		return err
 	}
 
-	err = rc.Flush()
+	err = srw.controller.Flush()
 	if err != nil {
 		return err
 	}
 
 	return nil
+}
+
+func NewStatusResponseWriter(writer http.ResponseWriter) *StreamResponseWriter {
+	return &StreamResponseWriter{
+		ResponseWriter: writer,
+		controller:     http.NewResponseController(writer),
+		statusCode:     http.StatusOK,
+	}
 }
 
 type HandlerContext struct {
@@ -55,7 +73,13 @@ func NewHandlerContext() *HandlerContext {
 	return &HandlerContext{clients: make(map[string]*Client)}
 }
 
-func (ctx *HandlerContext) StreamHandler(writer http.ResponseWriter, request *http.Request) {
+type HandlerFunc func(srw *StreamResponseWriter, request *http.Request)
+
+func (f HandlerFunc) ServeHTTP(srw *StreamResponseWriter, request *http.Request) {
+	f(srw, request)
+}
+
+func (ctx *HandlerContext) StreamHandler(srw *StreamResponseWriter, request *http.Request) {
 	defer func() {
 		delete(ctx.clients, request.RemoteAddr)
 		fmt.Printf("Client %s closed connection.\n", request.RemoteAddr)
@@ -73,19 +97,19 @@ func (ctx *HandlerContext) StreamHandler(writer http.ResponseWriter, request *ht
 
 	fmt.Printf("Handling incoming request from %s @ %d...\n", client.RemoteAddr, client.LastEventId)
 
-	rc := http.NewResponseController(writer)
-	writer.Header().Set("Content-Type", "text/event-stream")
-	writer.Header().Set("Cache-Control", "no-cache")
-	writer.Header().Set("Connection", "keep-alive")
-	writer.WriteHeader(http.StatusOK)
+	srw.Header().Set("Content-Type", "text/event-stream")
+	srw.Header().Set("Cache-Control", "no-cache")
+	srw.Header().Set("Connection", "keep-alive")
+	srw.WriteHeader(http.StatusOK)
 
-	if err := emit(writer, rc, "hello", fmt.Sprintf("Hello, %s!", client.RemoteAddr)); err != nil {
+	err := srw.WriteEvent("hello", fmt.Sprintf("Hello, %s!", client.RemoteAddr))
+	if err != nil {
 		return
 	}
 
 	for ; ; client.LastEventId++ {
 		random := GenerateRandomString(client.LastEventId, randomStringLength)
-		err := emit(writer, rc,
+		err := srw.WriteEvent(
 			fmt.Sprintf("%s%d", messageIdPrefix, client.LastEventId),
 			fmt.Sprintf("{\n  \"time\": %d,\n  \"random\": \"%s\"\n}", time.Now().Unix(), random))
 		if err != nil {
@@ -95,15 +119,32 @@ func (ctx *HandlerContext) StreamHandler(writer http.ResponseWriter, request *ht
 	}
 }
 
-func (ctx *HandlerContext) StatusHandler(writer http.ResponseWriter, request *http.Request) {
+func (ctx *HandlerContext) StatusHandler(srw *StreamResponseWriter, request *http.Request) {
 	body, err := json.Marshal(ctx.clients)
 	if err != nil {
-		writer.WriteHeader(http.StatusInternalServerError)
+		srw.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 
-	writer.Header().Set("Content-Type", "application/json")
-	_, _ = writer.Write(body)
+	srw.Header().Set("Content-Type", "application/json")
+	_, _ = srw.Write(body)
+}
+
+func loggerMiddleware(f HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		srw := NewStatusResponseWriter(writer)
+		start := time.Now()
+
+		defer func() {
+			fmt.Printf("%s %s %s %d (%v)\n",
+				start.Format(time.RFC3339),
+				request.Method,
+				request.URL,
+				srw.statusCode,
+				time.Since(start))
+		}()
+		f.ServeHTTP(srw, request)
+	}
 }
 
 func main() {
@@ -113,8 +154,8 @@ func main() {
 	}
 
 	ctx := NewHandlerContext()
-	http.HandleFunc("/stream", ctx.StreamHandler)
-	http.HandleFunc("/status", ctx.StatusHandler)
+	http.HandleFunc("/status", loggerMiddleware(ctx.StatusHandler))
+	http.HandleFunc("/stream", loggerMiddleware(ctx.StreamHandler))
 
 	fmt.Printf("Starting sse-server at :%s ...\n", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
